483cdfc84ecffe5d520c3c9d3d17b71f0ae5e8f1
Sebastian Gutierrez <jsgutierrezc@gmail.com>
2022-06-08 11:17:38 -0500

    Comunicamos con Base de Datos Mongo

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
-require('dotenv').config()
+const { json } = require('express')
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log('Petición recibida')
+mongoose
+    .connect(
+        `mongodb+srv://jsgutierrezc:${process.env.MONGO_DB_PASS}@webdevelopment.va1ay.mongodb.net/stock-app?retryWrites=true&w=majority`
+    )
+    .then((result) => {
+        app.listen(PORT, () => {
+            console.log('servidor escuchando en puerto ' + PORT)
+        })
+        console.log('conexión exitosa con la base de datos')
+    })
+    .catch((err) => console.log(err))
 
-    res.send('<h1>Hola mundo!</h1>')
-}
+const productSchema = mongoose.Schema(
+    {
+    name: { type:String, required: true},
+    price:Number,
+    },
+    { timestamps: true}
 )
 
-const PORT = process.env.PORT || 4000
+const Product = mongoose.model('Product', productSchema, 'Products')
 
-app.listen(PORT, () => {
+/*app.get('/', (req, res, next) => {
+    console.log('Petición recibida')
 
-    console.log('servidor escuchando en puerto ' + PORT)
+    next()
+})*/
 
+app.use(express.json())
+
+app.post('/api/v1/products', (req, res, next) => {
+    
+    const newProduct = new Product(req.body)
+    newProduct
+        .save()
+        .then((result) => {
+            res.status(201).json({ ok:true})
+        })
+        .catch((err) => console.log(err))    
 })
+
+app.use(express.static(path.join(__dirname,'public')))
+
+const PORT = process.env.PORT
+
+
 
